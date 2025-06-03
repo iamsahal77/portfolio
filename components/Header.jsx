eb83c8228ef377b3f15f66cf129707457ff45360
SAHAL. K.V. <116473073+iamsahal77@users.noreply.github.com>
2025-06-03 17:16:18 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     whileInView={{y: 0, opacity: 1}}
     transition={{duration: 0.8, delay: 0.5}}
     className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
-        frontend web developer based in India.</motion.h1>
+        frontend web developer crafting experiences.</motion.h1>
 
         <motion.p
         initial={{opacity: 0}}
